635f5c480fe10a9b02c0c206e1a6a005e1f49674
Hampus <hampus.hauffman@gmail.com>
2017-04-10 12:03:52 +0200

    first commit

---
+++ cmake-build-debug/CMakeFiles/TestC.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "D:/Programing/HI1026/TestC/main.c" "D:/Programing/HI1026/TestC/cmake-build-debug/CMakeFiles/TestC.dir/main.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "D:/Tools/CodeBlocks/MinGW/include/SDL2"
+  "D:/Programing/SDL/MinGW/SDL2_image-2.0.1/x86_64-w64-mingw32/include/SDL2"
+  "../"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
