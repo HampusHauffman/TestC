c9d7f2f7b5dcbf0a931b774474f3b68fffb0708b
Hampus <hampus.hauffman@gmail.com>
2017-04-11 10:09:58 +0200

    försök på att återställa

--- cmake-build-debug/CMakeFiles/TestC.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/TestC.dir/DependInfo.cmake
@@ -4,14 +4,14 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "D:/Dokument/ProgrammeringJacob/TestC/main.c" "D:/Dokument/ProgrammeringJacob/TestC/cmake-build-debug/CMakeFiles/TestC.dir/main.c.obj"
+  "D:/Programing/HI1026/TestC/main.c" "D:/Programing/HI1026/TestC/cmake-build-debug/CMakeFiles/TestC.dir/main.c.obj"
   )
 set(CMAKE_C_COMPILER_ID "GNU")
 
 # The include file search paths:
 set(CMAKE_C_TARGET_INCLUDE_PATH
-  "C:/Users/JacobHugo/Desktop/SDL/include/SDL2"
-  "C:/Users/JacobHugo/Desktop/SDL2_image-2.0.1/x86_64-w64-mingw32/include/SDL2"
+  "D:/Tools/CodeBlocks/MinGW/include/SDL2"
+  "D:/Programing/SDL/MinGW/SDL2_image-2.0.1/x86_64-w64-mingw32/include/SDL2"
   "../"
   )
 
